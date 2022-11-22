aacfe69a0b8413eacae40d6582398a3a7f2eeda7
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-22 16:01:12 +0100

    Partie 2 exercice 1 faite

--- tri_a_bulle.py
+++ tri_a_bulle.py
@@ -9,3 +9,15 @@ variableTemporaire = myTable[0]
 myTable[0] = myTable[1]
 myTable[1] = variableTemporaire
 
+##### PARTIE 2 #####
+
+# on arrete le parcours de tableau une iteration avant la fin 
+# car on ne peut pas comparer la derniere valeur avec une valeur qui n'existe pas (fin du tableau)
+for i in range(0, len(myTable)-1):
+
+    # si la valeur i est superieure a la valeur de la case suivante, on permute les deux cases
+    if(myTable[i] > myTable[i+1]):
+        variableTemporaire = myTable[i]
+        myTable[i] = myTable[i+1]
+        myTable[i+1] = variableTemporaire
+
