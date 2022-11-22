3f3c3342da09489d24cd6b76ff2c541c95c767b6
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-22 16:54:20 +0100

    Partie 3 de l'exercice 2 finie
    Bug a la ligne 50 (et surement 55 aussi) -> list index out of range
    a revoir

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -33,4 +33,29 @@ while (gameNotOver):
     for i in range(0, 3):
         print(grilleDeJeu[i][0], grilleDeJeu[i][1], grilleDeJeu[i][2])
 
+
+    ##### PARTIE 3 #####
+
+    # on verifie si un joueur n'a pas gagné en diagonale
+    # si c'est le cas, le jeu est fini et on affiche un message de victoire
+    if (grilleDeJeu[0][0] == grilleDeJeu[1][1] == grilleDeJeu[2][2]
+    or grilleDeJeu[0][2] == grilleDeJeu[1][1] == grilleDeJeu [0][2]):
+        gameNotOver = False
+        print("Bravo ! le joueur", (nbToursDeJeu+1)%2+1, "a gagne !")
+
+    # on verifie si un joueur n'a pas gagné en ligne
+    # si c'est le cas, le jeu est fini et on affiche un message de victoire
+    for i in range(0, 3):
+        # verification sur les lignes verticales
+        if ((grilleDeJeu[i][i] == grilleDeJeu[i][i+3]) and (grilleDeJeu[i][i] == grilleDeJeu[i][i+6])):
+            gameNotOver = False
+            print("Bravo ! le joueur", (nbToursDeJeu+1)%2+1, "a gagne !")
+
+        # verification sur les lignes horizontales
+        elif ((grilleDeJeu[i][i] == grilleDeJeu[i+3][i]) and  (grilleDeJeu[i][i] == grilleDeJeu[i+6][i])):
+            gameNotOver = False
+            print("Bravo ! le joueur", (nbToursDeJeu+1)%2+1, "a gagne !")
+
     nbToursDeJeu = nbToursDeJeu + 1
+
+
