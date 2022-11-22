a8bf1a150de844f071b9ecfcdf19b9dd4b67e364
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-22 17:04:40 +0100

    Modifications partie 3 exercice 2
    Bug aux lignes 50 et 55 reglé
    mais nouveau bug lignes 41-42 (le joueur gagne avec seulement deux cases alignées en diagonale)

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -38,23 +38,26 @@ while (gameNotOver):
 
     # on verifie si un joueur n'a pas gagné en diagonale
     # si c'est le cas, le jeu est fini et on affiche un message de victoire
-    if (grilleDeJeu[0][0] == grilleDeJeu[1][1] == grilleDeJeu[2][2]
-    or grilleDeJeu[0][2] == grilleDeJeu[1][1] == grilleDeJeu [0][2]):
+    if (((grilleDeJeu[0][0] == grilleDeJeu[1][1]) and (grilleDeJeu[0][0] == grilleDeJeu[2][2]))
+    or ((grilleDeJeu[0][2] == grilleDeJeu[1][1]) and (grilleDeJeu[0][2] == grilleDeJeu [0][2]))):
         gameNotOver = False
         print("Bravo ! le joueur", (nbToursDeJeu+1)%2+1, "a gagne !")
 
     # on verifie si un joueur n'a pas gagné en ligne
     # si c'est le cas, le jeu est fini et on affiche un message de victoire
-    for i in range(0, 3):
+    for i in range(0, 2):
         # verification sur les lignes verticales
-        if ((grilleDeJeu[i][i] == grilleDeJeu[i][i+3]) and (grilleDeJeu[i][i] == grilleDeJeu[i][i+6])):
+        if ((grilleDeJeu[i][i] == grilleDeJeu[i+1][i]) and (grilleDeJeu[i][i] == grilleDeJeu[i+2][i])):
             gameNotOver = False
             print("Bravo ! le joueur", (nbToursDeJeu+1)%2+1, "a gagne !")
+            break
 
         # verification sur les lignes horizontales
-        elif ((grilleDeJeu[i][i] == grilleDeJeu[i+3][i]) and  (grilleDeJeu[i][i] == grilleDeJeu[i+6][i])):
+        elif ((grilleDeJeu[i][i] == grilleDeJeu[i][i+1]) and  (grilleDeJeu[i][i] == grilleDeJeu[i][i+2])):
             gameNotOver = False
             print("Bravo ! le joueur", (nbToursDeJeu+1)%2+1, "a gagne !")
+            break
+
 
     ##### PARTIE 4 #####
 
